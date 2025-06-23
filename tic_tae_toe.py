1eaa5d8bd6fe03e049d8c3a57690878704e878b7
namansoni019 <156923007+namansoni019@users.noreply.github.com>
2025-06-23 14:37:48 +0530

    tic_tae_toe.py

--- tic_tae_toe.py
+++ tic_tae_toe.py
@@ -74,9 +74,14 @@ def get_move(symbol, board):
 
 def check_win(board, symbol):
     win = [
-        (0,1,2), (3,4,5), (6,7,8),
-        (0,3,6), (1,4,7), (2,5,8),
-        (0,4,8), (2,4,6)
+        (0,1,2), 
+        (3,4,5),
+        (6,7,8),
+        (0,3,6),
+        (1,4,7), 
+        (2,5,8),
+        (0,4,8), 
+        (2,4,6)
     ]
     for a,b,c in win:
         if board[a] == board[b] == board[c] == symbol:
